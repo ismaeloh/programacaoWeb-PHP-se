5abe9a6d3cbab1ca86d6442a2ed6ba7d3e7d3800
Ismael OH <110628422+ismaeloh@users.noreply.github.com>
2023-06-05 11:06:16 -0300

    primeira alteração

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilo.css">
-    <title>Programação PHP</title>
+    <title>IF ELSE - Programação PHP</title>
 
     <style type="text/css">
         .formulario{
